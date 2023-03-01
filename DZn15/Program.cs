851c8815ca2725fde5c071f692733869922892bb
Volodya <skochilovvladimir@yandex.ru>
2023-03-02 00:19:25 +0700

    thrd

---
+++ DZn15/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int dailyofWeek = Convert.ToInt32(Console.ReadLine());
+if (dailyofWeek == 6 || dailyofWeek == 7)
+    System.Console.WriteLine("Да");
+else
+    System.Console.WriteLine("Нет");
