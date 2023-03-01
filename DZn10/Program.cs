7234e5986af211279c37f87bd535b929bc7ec97d
Volodya <skochilovvladimir@yandex.ru>
2023-03-01 23:45:47 +0700

    first

---
+++ DZn10/Program.cs
@@ -0,0 +1,4 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int number = Convert.ToInt32(Console.ReadLine());
+int secondNumber =( number % 100 )/ 10;
+System.Console.WriteLine(secondNumber);
